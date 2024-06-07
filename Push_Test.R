68ccf3abe2d029e4f4f64bba7adabea84f1eda18
Taran Bhartt <Tarzor101@gmail.com>
2024-06-07 18:14:21 -0400

    This is the initial commit. This is a test script used to explore the Push function.

---
+++ Push_Test.R
@@ -0,0 +1,19 @@
+#' this is a test script
+#' The single quote after the hashtag makes this a command line
+#' And makes it easy to add more comments
+#' Until you delete the single quote
+# Then it won't work
+
+# see, it didn't add a comment
+#' Anyways, we'll do some code and then push it
+
+myvector <- seq(100, 1, by=-7)
+
+myavg <- mean(myvector)
+myavg
+
+mysum <- sum(myvector)
+mysum
+
+mysd <- sd(myvector)
+mysd
